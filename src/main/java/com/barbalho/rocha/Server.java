8d79a4204bdf3562c5370329c76676bdfd7818e3
Felipe Barbalho <felipebarbalho.95@gmail.com>
2020-07-16 20:18:43 -0300

    Connect with Apache MINA

--- src/main/java/com/barbalho/rocha/Server.java
+++ src/main/java/com/barbalho/rocha/Server.java
@@ -1,13 +1,40 @@
 package com.barbalho.rocha;
 
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.nio.NioTcpServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Server {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(Server.class);
+
+	public static void run() {
+		LOG.info("start server...");
+		final NioTcpServer acceptor = new NioTcpServer();
+		acceptor.setFilters(new LoggingFilter("LoggingFilter1"));
+		acceptor.setIoHandler(new ServerHandler());
+
+		try {
+
+			final SocketAddress address = new InetSocketAddress(9999);
+			acceptor.bind(address);
+			new BufferedReader(new InputStreamReader(System.in)).readLine();
+			acceptor.unbind();
+		} catch (final IOException e) {
+			LOG.error("Interrupted exception", e);
+		}
+	}
 
 	public static void main(String[] args) {
+		run();
+		/*
 		User user1 = new User(25, 61, 174, 6, "Felipe");
 		User user2 = new User(23, 80, 178, 4, "Luis");
 		Transaction transaction = null;
@@ -31,5 +58,6 @@ public class Server {
 			}
 			e.printStackTrace();
 		}
+		*/
 	}
 }
